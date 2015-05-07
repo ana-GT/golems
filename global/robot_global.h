d491c8936c98978f453570fe5b52b3b60671332a
Ana Huaman Quispe <ahuaman3@gatech.edu>
2015-05-06 20:47:55 -0400

    Added robot global header

---
+++ global/robot_global.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+
+enum ROBOT_TYPE {
+    LWA4_ROBOT = 0,
+    HUBO_ROBOT = 1,
+    ALITA_ROBOT = 2,
+    HUMAN_MODEL = 3,
+    TOTAL_ROBOT_TYPES = 4
+};
+
+enum HAND_TYPE {
+    SDH_HAND = 0,
+    LHUBO_HAND = 1,
+    RHUBO_HAND = 2,
+    ALITA_HAND = 3,
+    LHUMAN_HAND = 4,
+    RHUMAN_HAND = 5,
+    TOTAL_HAND_TYPES = 6
+};
