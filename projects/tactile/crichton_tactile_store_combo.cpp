2ebcf9b2586d6fcb79a091c144cb6a20b93b3eb8
Ana Huaman Quispe <ahuaman3@gatech.edu>
2015-06-08 23:17:16 -0400

    Added plotter for tactile that goes reasonably fast, instead of having 6 plots we have only one putting all together

--- projects/tactile/crichton_tactile_store_combo.cpp
+++ projects/tactile/crichton_tactile_store_combo.cpp
@@ -9,7 +9,7 @@
 ach_channel_t tactile_chan;
 FILE* pgnuplot;
 bool printHeader = false;
-int opt_frequency = 30/2;
+int opt_frequency = 30;
 
 /****/
 int main( int argc, char* argv[] ) {
@@ -23,8 +23,8 @@ int main( int argc, char* argv[] ) {
 
   // Start gnuplot
   pgnuplot = popen("gnuplot -persist", "w");
-  fprintf( pgnuplot, "set xrange [%f:%f]\n", -0.5,5.5 ); 
-  fprintf( pgnuplot, "set yrange [%f:%f]\n", -0.5,13.5 );
+  fprintf( pgnuplot, "set xrange [%f:%f]\n",-0.5,17.5 ); 
+  fprintf( pgnuplot, "set yrange [%f:%f]\n", -0.5,26.5 );
 
   fprintf( pgnuplot, "unset key\n" );
   fprintf( pgnuplot, "set tic scale 0\n" );
@@ -39,52 +39,63 @@ int main( int argc, char* argv[] ) {
  
   fprintf( pgnuplot, "set size 1,1\n" );
   fprintf( pgnuplot, "set origin 0,0\n" );
-//  fprintf( pgnuplot, "set multiplot layout 1,2 columnsfirst upwards title 'Pads'\n" );
 
 
   // Constantly read channel
   size_t frame_size;
   struct sns_msg_sdh_tactile* msg = 0;
+ 
+ 
   int height[6] = {14,13,14,13,14,13};
   int offset[6] = {0,84,162,246,324,408};
-  int width[6] = {6,6,6,6,6,6};
   int counter;
+  int p;
 
   while( true ) {
     r = sns_msg_local_get( &tactile_chan, (void**)&msg, &frame_size, NULL, ACH_O_LAST );
-       
 
     if( r == ACH_OK || r == ACH_MISSED_FRAME ) {
 
-      for( int k = 0; k < 6; k=k+6 ) {
-        
-        fprintf( pgnuplot, "set title 'Pad %d' \n", k );
+        fprintf( pgnuplot, "set title 'Pad' \n" );
         fprintf(pgnuplot, "splot '-' matrix with image \n");
-      
-        for( int y = height[k]-1; y >= 0; --y ) {
-          counter = offset[k] + y*width[k];  
-           for( int x = 0; x < width[k]; ++x ) {
-            int p; 
-            if( msg->x[counter] != 0 ) { p = 3000; } else { p = 0; }
-            fprintf( pgnuplot, "%d ", p );
-            counter++;
-          } 
+     
+        for( int y = 14 -1; y >= 0; y-- ) {
+          for( int finger = 1; finger <= 5; finger = finger + 2 ) {
+            counter = offset[finger] + y*6;
+            for( int x = 0; x < 6; ++x ) {
+              if( msg->x[counter] != 0 ) { p = 3000; } else { p = 0; }
+              fprintf( pgnuplot, "%d ", p );
+              counter++;              
+            }
+          }
+          fprintf( pgnuplot, "\n" );
+        }
+ 
+
+        for( int y = 13 -1; y >= 0; y-- ) {
+          for( int finger = 0; finger <= 4; finger = finger + 2 ) {
+            counter = offset[finger] + y*6;
+            for( int x = 0; x < 6; ++x ) {
+              if( msg->x[counter] != 0 ) { p = 3000; } else { p = 0; }
+              fprintf( pgnuplot, "%d ", p );
+              counter++;              
+            }
+          }
           fprintf( pgnuplot, "\n" );
         }
         fprintf( pgnuplot, "e\n");
         fprintf( pgnuplot, "e\n");       
-   fflush( pgnuplot ); 
-      } // for k
-
-   } else {
+        fflush( pgnuplot ); 
+      
+    } else {
      printf("Did not get the message for any reason \n");
    }
 
-   usleep( 0.03*1e6);
+   usleep( (1.0/opt_frequency)*1e6);
    aa_mem_region_local_release();
   } // end while
 
 
-  pclose( pgnuplot );
+  fclose( pgnuplot );
 
 }
--- projects/tactile/crichton_tactile_store_unit.cpp
+++ projects/tactile/crichton_tactile_store_unit.cpp
@@ -9,7 +9,7 @@
 ach_channel_t tactile_chan;
 FILE* pgnuplot;
 bool printHeader = false;
-int opt_frequency = 30/2;
+int opt_frequency = 30;
 
 /****/
 int main( int argc, char* argv[] ) {
@@ -23,7 +23,7 @@ int main( int argc, char* argv[] ) {
 
   // Start gnuplot
   pgnuplot = popen("gnuplot -persist", "w");
-  fprintf( pgnuplot, "set xrange [%f:%f]\n", -0.5,5.5 ); 
+  fprintf( pgnuplot, "set xrange [%f:%f]\n",-0.5,5.5 ); 
   fprintf( pgnuplot, "set yrange [%f:%f]\n", -0.5,13.5 );
 
   fprintf( pgnuplot, "unset key\n" );
@@ -39,31 +39,30 @@ int main( int argc, char* argv[] ) {
  
   fprintf( pgnuplot, "set size 1,1\n" );
   fprintf( pgnuplot, "set origin 0,0\n" );
-//  fprintf( pgnuplot, "set multiplot layout 1,2 columnsfirst upwards title 'Pads'\n" );
 
 
   // Constantly read channel
   size_t frame_size;
   struct sns_msg_sdh_tactile* msg = 0;
+ 
+  int opt_pad = 0;
+  if( argc == 2 ) { opt_pad = atoi( argv[1] ); }
+ 
   int height[6] = {14,13,14,13,14,13};
   int offset[6] = {0,84,162,246,324,408};
-  int width[6] = {6,6,6,6,6,6};
   int counter;
 
   while( true ) {
     r = sns_msg_local_get( &tactile_chan, (void**)&msg, &frame_size, NULL, ACH_O_LAST );
-       
 
     if( r == ACH_OK || r == ACH_MISSED_FRAME ) {
 
-      for( int k = 0; k < 6; k=k+6 ) {
-        
-        fprintf( pgnuplot, "set title 'Pad %d' \n", k );
+        fprintf( pgnuplot, "set title 'Pad %d' \n", opt_pad );
         fprintf(pgnuplot, "splot '-' matrix with image \n");
       
-        for( int y = height[k]-1; y >= 0; --y ) {
-          counter = offset[k] + y*width[k];  
-           for( int x = 0; x < width[k]; ++x ) {
+        for( int y = height[opt_pad] -1; y >= 0; --y ) {
+          counter = offset[opt_pad] + y*6;
+          for( int x = 0; x < 6; ++x ) {
             int p; 
             if( msg->x[counter] != 0 ) { p = 3000; } else { p = 0; }
             fprintf( pgnuplot, "%d ", p );
@@ -71,20 +70,22 @@ int main( int argc, char* argv[] ) {
           } 
           fprintf( pgnuplot, "\n" );
         }
+   
         fprintf( pgnuplot, "e\n");
-        fprintf( pgnuplot, "e\n");       
-   fflush( pgnuplot ); 
-      } // for k
-
-   } else {
+        fprintf( pgnuplot, "e\n");
+          
+      
+    } else {
      printf("Did not get the message for any reason \n");
    }
 
-   usleep( 0.03*1e6);
+   fflush( pgnuplot ); 
+
+   usleep( (1.0/opt_frequency)*1e6);
    aa_mem_region_local_release();
   } // end while
 
 
-  pclose( pgnuplot );
+  fclose( pgnuplot );
 
 }
