6221959852313b7bf03c914623800b396e21adda
Ana Huaman Quispe <ahuaman3@gatech.edu>
2015-07-07 23:12:39 -0400

    Added deformation tests

---
+++ perception/pointcloud_tools/sq_fitting/tests/deformations_tests_0.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file deformations_tests_0.cpp
+ * @brief Testing simple 1-DOF bending deformation
+ */
+#include "evaluated_eqs.h"
+#include <SQ_utils.h>
+#include <SQ_deformations.h>
+
+/**
+ * @function main
+ */
+int main( int argc, char*argv[] ) {
+
+  double Rfactor = 1.4;
+  double tamp = 0.5;
+  
+  int v;
+  while( (v=getopt(argc, argv, "t:b:")) != -1 ) {
+    switch(v) {
+
+    case 't' : {
+      tamp = atof(optarg);
+    } break;
+    case 'b' : {
+      Rfactor = atof(optarg);
+    } break;
+    } // switch end
+  }
+
+  
+  SQ_deformations sd;
+
+  double a3 = 0.1;
+  double a2 = 0.025;
+  double a1 = 0.025;
+  
+  sd.bending( a1, a2, a3, 0.1, 1.0, Rfactor*a3);
+  sd.tampering( a1, a2, a3, 0.1, 1.0, tamp);
+  
+  return 0;  
+}
---
+++ perception/pointcloud_tools/sq_fitting/tests/deformations_tests_1.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file deformations_tests_1.cpp
+ * @brief Testing bending
+ */
+#include "evaluated_eqs.h"
+#include <SQ_utils.h>
+#include <SQ_deformations.h>
+
+/**
+ * @function main
+ */
+int main( int argc, char*argv[] ) {
+
+  double k, alpha;
+  std::string name = std::string("default_b2.pcd");
+  
+  k = 1.0; alpha = 0.87;
+  
+  int v;
+  while( (v=getopt(argc, argv, "k:a:n:h")) != -1 ) {
+    switch(v) {
+
+    case 'k' : {
+      k = atof(optarg);
+    } break;
+    case 'a' : {
+      alpha = atof(optarg);
+    } break;
+    case 'n' : {
+      name = std::string(optarg);
+    } break;
+    case 'h' : {
+      printf("Syntax: ./executable -k K_bending -a alpha_bending -n name_bent_pcd_output\n");
+      return 0;
+    } break;
+    } // switch end
+  }
+
+  
+
+  SQ_parameters par;
+  par.dim[0] = 0.03; par.dim[1] = 0.03; par.dim[2] = 0.12;
+  par.e[0] = 0.1; par.e[1] = 1.0;
+  par.trans[0] = 0; par.trans[1] = 0; par.trans[2] = 0;
+  par.rot[0] = 0; par.rot[1] = 0; par.rot[2] = 0;
+  par.alpha = alpha; par.k = k;
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr approx( new pcl::PointCloud<pcl::PointXYZ>() );
+  pcl::PointCloud<pcl::PointXYZ>::Ptr really( new pcl::PointCloud<pcl::PointXYZ>() );
+  approx = sampleSQ_uniform_b2( par );
+  really = sampleSQ_uniform(par);
+
+  printf("ALpha: %f \n", par.alpha);
+
+  pcl::io::savePCDFileASCII( name, *approx );
+  pcl::io::savePCDFileASCII( "real.pcd", *really );
+  
+  return 0;  
+}
